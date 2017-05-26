9f6e94072bdf0753bc8cb9aa6ef387b526b06a08
Jennifer Wilson <jennwilson55@gmail.com>
2017-05-26 02:35:16 +0000

    loops all passing

--- loops.js
+++ loops.js
@@ -14,3 +14,13 @@ function whileLoop(num) {
   }
   return 'done'
 }
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+  console.log('doo-bee-doo-bee-doo')
+  } while (maybeTrue());
+  array.pop();
+  return array
+}
